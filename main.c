0020329aca7a9bac78e4d1628f4fcc46bca9485f
goosecoid <adblavier@gmail.com>
2024-06-12 08:17:49 +0200

    fix: misc stuff

--- main.c
+++ main.c
@@ -26,21 +26,23 @@ bool isTimerDone(Timer *timer) {
 }
 
 int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  const int screenWidth = 225;
+  const int screenHeight = 225;
   const int startAngle = 270.0f;
   bool start = false;
   int endAngle = -90.0f;
   Timer timer = {0};
   int minutes = 25;
   int counter = 60 * minutes;
-  const float increment = 360.0f / counter;
+  float increment = 360.0f / counter;
+  Color ringColor = RED;
+  int cycle = 1;
 
   // Buffer to dynamically allocate text for the timer
   char *buf;
   size_t sz;
 
-  InitWindow(screenWidth, screenHeight, "Raylib");
+  InitWindow(screenWidth, screenHeight, "Pomodoro Timer");
 
   SetTargetFPS(60);
 
@@ -59,7 +61,27 @@ int main(void) {
     }
 
     if (counter == 0) {
+      if (minutes == 3 && cycle < 4) {
+        minutes = 5;
+        counter = 60 * minutes;
+        increment = 360.0f / counter;
+        ringColor = GREEN;
+      } else if (minutes == 3 && cycle == 4) {
+        minutes = 20;
+        counter = 60 * minutes;
+        increment = 360.0f / counter;
+        ringColor = GREEN;
+        cycle = 0;
+      } else {
+        cycle++;
+        minutes = 25;
+        counter = 60 * minutes;
+        increment = 360.0f / counter;
+        ringColor = RED;
+      }
       SetWindowFocused();
+      start = false;
+      endAngle = -90.0f;
     }
 
     // Set the buffer to the formatted string
@@ -68,15 +90,16 @@ int main(void) {
     snprintf(buf, sz + 1, "%02d:%02d", counter / 60, counter % 60);
 
     // Start/Pause button
-    if (GuiButton((Rectangle){GetScreenWidth() / 2.0f - 57,
-                              GetScreenHeight() / 2.0f + 20, 120, 30},
-                  start ? "Pause" : "Start")) {
+    if (GuiButton((Rectangle){GetScreenWidth() / 2.0f - 10,
+                              GetScreenHeight() / 2.0f + 20, 20, 20},
+                  start ? GuiIconText(ICON_PLAYER_PAUSE, "")
+                        : GuiIconText(ICON_PLAYER_PLAY, ""))) {
       start = !start;
     }
 
     // Reset button
     if (GuiButton((Rectangle){GetScreenWidth() / 2.0f - 10,
-                              GetScreenHeight() / 2.0f + 60, 20, 20},
+                              GetScreenHeight() / 2.0f + 50, 20, 20},
                   GuiIconText(ICON_UNDO, ""))) {
       counter = 60 * minutes;
       endAngle = -90.0f;
@@ -90,7 +113,7 @@ int main(void) {
              GetScreenHeight() / 2.0f - (pos.y / 2) - 20, 40, BLACK);
 
     DrawRing((Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f}, 90,
-             100, startAngle, endAngle, 360, RED);
+             100, startAngle, endAngle, 360, ringColor);
     EndDrawing();
   }
 
